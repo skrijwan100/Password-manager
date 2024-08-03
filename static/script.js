2c8cd4b3264ac92ed951fc8f83ce6e3738ae9ba9
skrijwan <skrijwan329@gmail.com>
2024-08-03 22:09:17 +0530

    uplod file

--- static/script.js
+++ static/script.js
@@ -1,14 +1,3 @@
 let visibale = document.getElementsByClassName("visibale")
 console.log(visibale)
-function showpassword() {
-    let storepass = document.getElementById("storepass")
-    visibale.style.display = "none"
-    if (storepass.type == "password") {
-        storepass.type = "text"
-    }
-    else {
-        storepass.type = "password"
-
-    }
-}
 
