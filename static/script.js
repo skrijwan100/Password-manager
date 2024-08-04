46132915455f119fe8a13107ffb517fa1f35c4cf
skrijwan <skrijwan329@gmail.com>
2024-08-04 10:37:27 +0530

    some upgrade

--- static/script.js
+++ static/script.js
@@ -1,3 +1,12 @@
-let visibale = document.getElementsByClassName("visibale")
-console.log(visibale)
+
+function togglePassword(id) {
+    const passwordInput = document.getElementById(id);
+    if(passwordInput.type=="password"){
+        passwordInput.type="text"
+    }
+    else{
+        passwordInput.type="password"
+        
+    }
+}
 
